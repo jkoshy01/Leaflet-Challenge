a7e13768eb808657c736d49da3eed0dcb3851f6c
Jaimie <69375588+jkoshy01@users.noreply.github.com>
2021-01-28 19:57:43 -0500

    Update logic.js

--- logic.js
+++ logic.js
@@ -12,7 +12,7 @@ var myMap = L.map("map", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
     id: "mapbox.streets",
-    accessToken: "pk.eyJ1IjoiY2hpYW15YzA5ODciLCJhIjoiY2swdzUxb3I2MGRiMzNpbnliN293OXBteiJ9.at8rk5Trv5oNH1dD2E9EAw"
+    accessToken: "pk.eyJ1Ijoiamtvc2h5MDEiLCJhIjoiY2trNjRocXo5MDBnMTJ3b2ViaGRma3d5OCJ9.66D3HjyATBPEm18z9GpcqQ
   }).addTo(myMap);
 
   // Store our API endpoint
